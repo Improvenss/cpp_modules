ffb4534175091aac60db69125b396823c6decf34
Gorkem Sever <gsever@k2m06s02.42kocaeli.com.tr>
2022-12-27 18:16:01 +0300

    module01->ex00 OK, ex02 on process

--- module01/ex00/megaphone.cpp
+++ module01/ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: gsever <gsever@student.42kocaeli.com.tr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/27 15:32:39 by gsever            #+#    #+#             */
-/*   Updated: 2022/12/27 15:59:40 by gsever           ###   ########.fr       */
+/*   Updated: 2022/12/27 18:15:18 by gsever           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,10 +20,33 @@
  * 	Meaning, inside standart library's cout func().
  * std::endl => writing newline(\n)
  * 
+ * toupper(int _c); -> Converting to upper with decimal value!.
+ * 	If you don't write "type casting" -> (char) it's returning decimal value.
+ * 	If you write type casting -> (char)toupper(int _c); -> returning char value.
+ * 
  * @return int 
  */
-int	main()
+int	main(int argc, char **argv)
 {
-	std::cout << "Merhaba benim adim Nqme!" << std::endl;
+	int	i;
+	int	j;
+
+	if (argc == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	else if (argc > 1)
+	{
+		i = 1;
+		while (argv[i] != NULL)
+		{
+			j = 0;
+			while (argv[i][j] != '\0')
+			{
+				std::cout << (char)toupper(argv[i][j]);
+				j++;
+			}
+			i++;
+		}
+		std::cout << std::endl;
+	}
 	return (0);
 }
