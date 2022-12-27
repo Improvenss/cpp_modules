c3dd4742d273296a18a44b6b401bc451cb0af064
Gorkem Sever <gsever@k2m06s02.42kocaeli.com.tr>
2022-12-27 16:24:02 +0300

    Makefile OK

---
+++ module01/ex00/megaphone.cpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gsever <gsever@student.42kocaeli.com.tr    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/27 15:32:39 by gsever            #+#    #+#             */
+/*   Updated: 2022/12/27 15:59:40 by gsever           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>// Basic functions inside this library.
+// using namespace std;
+
+/**
+ * @brief 
+ * 
+ * std::cout => standart->cout => standart->c_language_out(1);
+ * 	Meaning, inside standart library's cout func().
+ * std::endl => writing newline(\n)
+ * 
+ * @return int 
+ */
+int	main()
+{
+	std::cout << "Merhaba benim adim Nqme!" << std::endl;
+	return (0);
+}
