3eb1617c3e1aba49156e2588dba3ddc9711dc862
Gorkem Sever <gsever@k2m12s01.42kocaeli.com.tr>
2022-12-30 16:36:13 +0300

    ex02 started sir.

---
+++ module01/ex02/Account.cpp
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Account.cpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gsever <gsever@student.42kocaeli.com.tr    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/30 13:37:42 by gsever            #+#    #+#             */
+/*   Updated: 2022/12/30 16:35:04 by gsever           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Account.hpp"
+#include <iostream>
+#include <ctime>
+#include <iomanip>
+
+Account::Account(int initial_deposit)
+{
+	(void)initial_deposit;
+}
+
+Account::~Account(void)
+{
+
+}
+int	Account::getNbAccounts(void)
+{
+	return (_nbAccounts);
+}
+
+int	Account::getTotalAmount(void)
+{
+	return (_totalAmount);
+}
+
+int	Account::getNbDeposits(void)
+{
+	return (_totalNbDeposits);
+}
+
+int	Account::getNbWithdrawals(void)
+{
+	return (_totalNbWithdrawals);
+}
+
+void	Account::displayAccountsInfos(void)
+{
+
+}
