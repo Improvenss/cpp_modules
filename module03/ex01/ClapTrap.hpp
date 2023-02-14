b396bd71f4b3de949a33967b01c5e05578081c11
Görkem Sever <gorkem_se_98@hotmail.com>
2023-02-15 01:07:33 +0300

    ex01 started:

---
+++ module03/ex01/ClapTrap.hpp
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gsever <gsever@student.42kocaeli.com.tr    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/13 18:34:58 by gsever            #+#    #+#             */
+/*   Updated: 2023/02/14 22:43:52 by gsever           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CLAPTRAP_HPP
+# define CLAPTRAP_HPP
+
+#include <iostream>
+
+# define BLACK		"\033[0;30m"
+# define RED		"\033[0;31m"
+# define GREEN		"\033[0;32m"
+# define YELLOW		"\033[0;33m"
+# define BLUE		"\033[0;34m"
+# define PURPLE		"\033[0;35m"
+# define CYAN		"\033[0;36m"
+# define WHITE		"\033[0;37m"
+# define END		"\033[m"
+# define RESET		"\033[0m"
+
+class ClapTrap
+{
+	private:
+		std::string		_name;
+		unsigned int	_hitPoints;
+		unsigned int	_energyPoints;
+		unsigned int	_attackDamage;
+	public:
+		ClapTrap( void );
+		ClapTrap( std::string name );
+		ClapTrap( const ClapTrap &rhs );
+		ClapTrap	&operator=( const ClapTrap &rhs );
+		~ClapTrap();
+
+		void	attack( const std::string &target);// ok
+		void	takeDamage( unsigned int amount );// ok
+		void	beRepaired( unsigned int amount );// ok
+		bool	isALive( void );// ok
+		void	setDamage( unsigned int amount );// ok
+};
+
+#endif
---
+++ module03/ex01/main.cpp
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gsever <gsever@student.42kocaeli.com.tr    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/13 18:32:08 by gsever            #+#    #+#             */
+/*   Updated: 2023/02/15 01:06:53 by gsever           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ClapTrap.hpp"
+
+int	main()
+{
+	ClapTrap	p1;
+	ClapTrap	p2("player2");
+	ClapTrap	p3("player3");
+	ClapTrap	p4(p2);
+	ClapTrap	p5 = p3;
+
+	p4 = p5;
+	std::cout << "-------- Players created! ------" << std::flush << std::endl << std::endl;
+	while (p1.isALive() && p2.isALive() && p3.isALive())
+	{
+		p1.attack("player2");
+		p1.setDamage(5);
+		p2.takeDamage(5);
+		std::cout << std::endl;
+	}
+	std::cout << std::endl;
+	p1.beRepaired(10);
+	std::cout << std::endl;
+	p2.beRepaired(10);
+	std::cout << std::endl;
+	std::cout << "-------- Players Destroying! ------" << std::flush << std::endl;
+	return (0);
+}
