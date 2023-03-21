7880eeaa422d14d472927ab37898438c79e3e18c
Görkem Sever <gorkem_se_98@hotmail.com>
2023-03-21 14:59:14 +0300

    Added 10.000 numbers test.

--- module08/ex01/main.cpp
+++ module08/ex01/main.cpp
@@ -6,11 +6,44 @@
 /*   By: gsever <gsever@student.42kocaeli.com.tr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/20 12:39:33 by gsever            #+#    #+#             */
-/*   Updated: 2023/03/21 14:54:08 by gsever           ###   ########.fr       */
+/*   Updated: 2023/03/21 14:58:42 by gsever           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Span.hpp"
+
+void	selfPartSpanTest2( void )
+{
+	std::cout << B_BLUE "------------- Self Test Area! ------------" END\
+		<< std::flush << std::endl;
+	try
+	{
+		std::list<int>	listSpan(10000); // Creating 10.000 number count(N) <int> list.
+		Span::printList(&listSpan, "std::list<int>listSpan(10): ");
+
+		std::srand(time(NULL)); // Creating numbers randomly.
+		std::generate(listSpan.begin(), listSpan.end(), std::rand); // Generating list's begin to end random number.
+		Span::printList(&listSpan, "std::list<int>listSpan(generate): ");
+
+		Span	sp(listSpan.size()); // Creating Span class; have 10.000 number range.
+		sp.printList(sp.getList(), "Span(sp): ");
+
+		sp.addNumber(listSpan.begin(), listSpan.end()); // Adding these randomly created numbers in to the 'sp' class's list.
+		sp.printList(sp.getList(), "Span(sp) addNumber(): ");
+
+		std::cout << "selfSpan: shortestSpan: " << sp.shortestSpan()\
+			<< std::flush << std::endl;
+		std::cout << "selfSpan: longestSpan: " << sp.longestSpan()\
+			<< std::flush << std::endl;
+	}
+	catch (const std::exception &e)
+	{
+		std::cerr << B_RED << e.what() << END << std::flush << std::endl;
+	}
+	std::cout << B_BLUE "------------------------------------------" END\
+		<< std::flush << std::endl;
+}
+
 
 void	selfPartSpanTest( void )
 {
@@ -73,6 +106,7 @@ int	main()
 		<< std::flush << std::endl;
 	mendatoryPartSpanTest(sp);
 	selfPartSpanTest();
+	// selfPartSpanTest2();
 	std::cout << "---------- Span List Destroying! ---------"\
 		<< std::flush << std::endl;
 	return (0);
