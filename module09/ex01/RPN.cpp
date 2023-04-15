9d66f407a4e02118ee268966ddc57674c89df454
Görkem Sever <gorkem_se_98@hotmail.com>
2023-04-15 20:42:45 +0300

    Module-09 ex01 finished.

--- module09/ex01/RPN.cpp
+++ module09/ex01/RPN.cpp
@@ -6,17 +6,71 @@
 /*   By: gsever <gsever@student.42kocaeli.com.tr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/12 21:34:59 by gsever            #+#    #+#             */
-/*   Updated: 2023/04/12 21:50:08 by gsever           ###   ########.fr       */
+/*   Updated: 2023/04/15 20:40:50 by gsever           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "RPN.hpp"
 
-RPN::RPN( void )
+RPN::RPN( void ) {}
+
+RPN::RPN( char *argv ) : _input(argv) {}
+
+RPN::~RPN( void ) {}
+
+/* _________________________ MAIN FUCTION ___________________________________ */
+
+void	RPN::calculateArray( void )
+{
+	for (std::string::iterator	it = this->_input.begin();
+		it != this->_input.end(); it++)
+	{
+		// char	c = static_cast<char>(*it); // Converted iterator to char.
+		this->_operator = static_cast<char>(*it); // Converted iterator to char.
+		// std::cout << "reading: [" << c << "]" << std::flush << std::endl;
+		// std::cout << "type: " << typeof(_operator).name() << std::flush << std::endl;
+		if (std::isdigit(this->_operator))
+			this->_stack.push(this->_operator - '0'); // Converted char to int and add to _stack.
+		else if (!std::isspace(this->_operator)) // Checking if not isspace.
+		{
+			if (this->_stack.size() < 2)
+				throw (std::invalid_argument("Unexpected character."));
+			// std::cout << "stack.top(): " << this->_stack.top() << std::flush << std::endl;
+			int	rhsNumber = this->_stack.top();
+			this->_stack.pop();
+			int	lhsNumber = this->_stack.top();
+			this->_stack.pop();
+			switch (this->_operator)
+			{
+			case '+':
+				this->_stack.push(lhsNumber + rhsNumber);
+				break;
+			case '-':
+				this->_stack.push(lhsNumber - rhsNumber);
+				break;
+			case '/':
+				this->_stack.push(lhsNumber / rhsNumber);
+				break;
+			case '*':
+				this->_stack.push(lhsNumber * rhsNumber);
+				break;
+			default:
+				throw (std::invalid_argument("Invalid operator. You can use [+, -, /, *]."));
+				// std::cerr << "Error" << std::flush << std::endl;
+				// exit(EXIT_FAILURE);
+				break;
+			}
+			if (this->_stack.size() != 1)
+				throw (std::out_of_range("Calculate problem."));
+		}
+	}
+	std::cout << this->_stack.top() << std::flush << std::endl;
+}
+/* -------------------------------------------------------------------------- */
+
+/* _________________________ PRINT FUCTIONS _________________________________ */
+
+void	printArrayTopIterator( void )
 {
 }
-
-RPN::~RPN( void )
-{
-}
-
+/* -------------------------------------------------------------------------- */
--- module09/ex01/main.cpp
+++ module09/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: gsever <gsever@student.42kocaeli.com.tr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/12 21:34:46 by gsever            #+#    #+#             */
-/*   Updated: 2023/04/12 23:03:12 by gsever           ###   ########.fr       */
+/*   Updated: 2023/04/15 20:37:08 by gsever           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,14 +24,40 @@ int	main( int argc, char **argv )
 	}
 	try
 	{
-		/* code */
-		(void)argv;
-		
-		// while (true);
+		RPN	polishMath(argv[1]);
+		polishMath.calculateArray();
 	}
 	catch(const std::exception& e)
 	{
-		std::cerr << "RPN: Error:" << e.what() << std::flush << std::endl;
+		std::cerr << "RPN: Error: " << e.what() << std::flush << std::endl;
 	}
 	return (EXIT_SUCCESS);
 }
+
+/**
+ * @brief PDF en to tr.
+ * 
+Bu kısıtlamalara sahip bir program oluşturmalısınız:
+• Programın adı RPN'dir.
+• Programınız argüman olarak ters çevrilmiş bir Lehçe matematiksel ifade almalıdır.
+• Bu işlemde kullanılan sayılar her zaman 10'dan küçük olacaktır.
+• Programınız bu ifadeyi işlemeli ve ekranda doğru sonucu vermelidir.
+standart çıktı.
+• Programın yürütülmesi sırasında bir hata oluşursa, standart çıktıda bir hata mesajı görüntülenmelidir.
+• Programınız şu belirteçlerle işlemleri gerçekleştirebilmelidir: "+ - / *".
+
+Parantezleri veya ondalık sayıları yönetmenize gerek yoktur.
+
+$> ./RPN "8 9 * 9 - 9 - 9 - 4 - 1 +"
+42
+$> ./RPN "7 7 * 7 -"
+42
+$> ./RPN "(1 + 1)"
+Error
+$>
+
+Uyarı: Bir önceki alıştırmada kullandığınız kap(lar)
+burada yasak. Bu alıştırmayı doğrulamak için kullandığınız kapsayıcı(lar)
+ bu modülün geri kalanı için kullanılamaz.
+ * 
+ */
