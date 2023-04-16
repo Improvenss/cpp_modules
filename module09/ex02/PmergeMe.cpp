ff0953aee2b372c2d74c03fa1e6db76ee819ce19
Görkem Sever <gorkem_se_98@hotmail.com>
2023-04-16 22:21:42 +0300

    ex02 in process

--- module09/ex02/PmergeMe.cpp
+++ module09/ex02/PmergeMe.cpp
@@ -6,9 +6,23 @@
 /*   By: gsever <gsever@student.42kocaeli.com.tr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/15 20:46:11 by gsever            #+#    #+#             */
-/*   Updated: 2023/04/15 20:46:34 by gsever           ###   ########.fr       */
+/*   Updated: 2023/04/16 15:54:02 by gsever           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "PmergeMe.hpp"
 
+PmergeMe::PmergeMe( void ) {}
+
+PmergeMe::PmergeMe( int argc, char **argv )
+{
+	(void)argc;
+	(void)argv;
+}
+
+PmergeMe::~PmergeMe( void ) {}
+
+void	PmergeMe::executePmergeSort( void )
+{
+
+}
--- module09/ex02/main.cpp
+++ module09/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: gsever <gsever@student.42kocaeli.com.tr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/15 20:43:48 by gsever            #+#    #+#             */
-/*   Updated: 2023/04/15 20:46:36 by gsever           ###   ########.fr       */
+/*   Updated: 2023/04/16 15:55:25 by gsever           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,74 @@
 
 int	main( int argc, char **argv )
 {
-
+	try
+	{
+		if (argc <= 1)
+			throw (std::invalid_argument("You must be enter ./PmergeMe\
+ 3 5 9 7 4"));
+		PmergeMe	pmerge(argc, argv);
+		pmerge.executePmergeSort();
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << RED "PmergeMe: Error: " << e.what() << END\
+			<< std::flush << std::endl;
+		return (EXIT_FAILURE);
+	}
+	return (EXIT_SUCCESS);
 }
+
+/**
+ * @brief PDF en to tr
+ * 
+Bu kısıtlamalara sahip bir program oluşturmalısınız:
+
+• [OK] Programın adı PmergeMe'dir.
+• [] Programınız 'POZITIF' bir tamsayı dizisini bağımsız değişken olarak
+ kullanabilmelidir.
+• [] Programınız, 'POZITIF' tamsayı sırasını sıralamak için birleştirme-ekleme
+ sıralama algoritması kullanmalıdır.
+• [] Programın yürütülmesi sırasında bir hata oluşursa, standart çıktıda bir hata
+ mesajı görüntülenmelidir.
+
+Kodunuzda en az iki farklı kap kullanmalısınız.
+bu alıştırmayı onaylayın. Programınız şu adreste işleyebilmelidir:
+en az 3000 farklı tamsayı.
+
+Algoritmanızı her kapsayıcı için uygulamanız şiddetle tavsiye edilir.
+ve böylece genel bir işlev kullanmaktan kaçınmak için.
+
+Burada, standart çıktıda satır satır görüntülemeniz gereken bilgilerle ilgili
+ bazı ek yönergeler verilmiştir.
+• İlk satırda açık bir metin ve ardından sıralanmamış 'POZITIF' tamsayı dizisini
+ görüntülemeniz gerekir.
+• İkinci satırda, açık bir metni ve ardından sıralanmış 'POZITIF' tamsayı dizisini
+ görüntülemeniz gerekir.
+• Üçüncü satırda, 'POZITIF' tamsayı dizisini sıralamak için kullanılan ilk
+ kapsayıcıyı belirterek, algoritmanız tarafından kullanılan zamanı gösteren açık
+ bir metin göstermelisiniz.
+• Son satırda, 'POZITIF' tamsayı dizisini sıralamak için kullanılan ikinci
+ kapsayıcıyı belirterek, algoritmanız tarafından kullanılan zamanı gösteren açık
+ bir metin göstermelisiniz.
+
+Sıralamanızı gerçekleştirmek için kullanılan zamanın görüntülenme biçimi
+ ücretsizdir ancak seçilen kesinlik, kullanılan iki kap arasındaki farkı net
+ bir şekilde görmeye izin vermelidir.
+
+$> ./PmergeMe 3 5 9 7 4 Before: 35974
+After: 34579
+Time to process a range of 5 elements with std::[..] : 0.00031 us
+Time to process a range of 5 elements with std::[..] : 0.00014 us $> ./PmergeMe `shuf -i 1-100000 -n 3000 | tr "\n" " "`
+Before:  141 79 526 321 [...]
+After:   79 141 321 526 [...]
+Time to process a range of 3000 elements with std::[..] : 62.14389 us
+Time to process a range of 3000 elements with std::[..] : 69.27212 us
+$> ./PmergeMe "-1" "2"
+Error
+$>
+
+Uyarı: Önceki alıştırmalarda kullandığınız kap(lar) burada yasak.
+
+Yinelemelerle ilgili hataların yönetimi sizin takdirinize bırakılmıştır.
+ * 
+ */
